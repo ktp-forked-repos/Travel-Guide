4bfc75193105651e656aa434a18fbe63718927e0
sotiriskarageorgopoulos <32032584+sotiriskarageorgopoulos@users.noreply.github.com>
2018-02-20 21:48:43 +0200

    Create Travel_guide.pl

---
+++ Travel_guide.pl
@@ -0,0 +1,155 @@
+island('NAXOS','POLICE_TEL.2285022100','FD_TEL.2285032199','Hospital_TEL.2285360500','Coastguard_TEL.2285022300').
+island('SANTORINI','POLICE_TEL.2286022649','FD_TEL.2286022657','Hospital_TEL.2286035300','Coastguard_TEL.2286022239').
+island('RHODES','POLICE_TEL.2241023278','FD_TEL.22410-23333','Hospital_TEL.2241360000','Coastguard_TEL.2241022220').
+island('MYKONOS','POLICE_TEL.22890 22716','FD_TEL.22813-61901','Hospital_TEL.2289360000','Coastguard_TEL.2285022300').
+mountainous('ARACHOVA','POLICE_TEL.22670 31333','FD_TEL.22610-28555','Hospital_TEL.2262350100').
+mountainous('ZAGOROCHORIA','POLICE_TEL.2262350100','FD_TEL.2262350100','Hospital_TEL.2651 366111').
+mountainous('METSOVO','POLICE_TEL.26560 41233','FD_TEL.2656041199','Hospital_TEL.2656041111').
+seaside('PYLOS','POLICE_TEL.27230 22316','FD_TEL.2723022099','Hospital_TEL.2723360000','Coastguard_TEL.2723022225').
+seaside('CHALKIDIKI','POLICE_TEL.23750 32410','FD_TEL.2371022673','Hospital_TEL.2371350100','Coastguard_TEL.2377071248').
+seaside('MONEMVASIA','POLICE_TEL.2732061941','FD_TEL.27320-23888','Hospital_TEL.2732360100','Coastguard_TEL.2732061266').
+museums_sea('PYLOS','ARCHEOLOGICAL MUSEUM OF PYLOS','-').
+museums_sea('CHALKIDIKI','HISTORICAL MUSEUM','FOLKORE MUSEUM OF POLYXRONOY').
+museums_sea('MONEMVASIA','ARCHEOLOGICAL MUSEUM OF MONEMVASIAS','-').
+museums_isl('SANTORINI','ARCHEOLOGICAL MUSEUM OF THIRA','MUSEUM OF PREHISTORIC').
+museums_isl('RHODES','ARCHEOLOGICAL MUSEUM OF RHODES','ART GALLERY OF RHODES').
+museums_isl('NAXOS','ARCHEOLOGICAL MUSEUM OF NAXOS','BYZANTINE MUSEUM OF NAXOS').
+museums_isl('MYKONOS','ARCHEOLOGICAL MUSEUM OF MYKONOS','FOLKORE MUSEUM OF MYKONOS').
+museums_mo('ARACHOVA','FOLKORE MUSEUM OF ARAXOVA','-').
+museums_mo('ZAGOROCHORIA','FOLKORE MUSEUM','-').
+museums_mo('METSOVO','FOLKORE MUSEUM OF METSOVO','-').
+archeological_sites_sea('PYLOS','ANCIENT MESSINI','ANCIENT THEATER OF MESSINI').
+archeological_sites_sea('CHALKIDIKI','THE PARK OF ARISTOTELIS','ANCIENT AKANTHOS').
+archeological_sites_sea('MONEMVASIA','THE CATLES OF MONEMVASIA','-').
+archeological_sites_isl('SANTORINI','ARCHEOLOGICAL REGION OF ANCIENT THIRA','-').
+archeological_sites_isl('RHODES','CITADEL OF RHODES','ANCIENT STADIUM').
+archeological_sites_isl('MYKONOS','ARCHED ΤΟΜΒ','-').
+archeological_sites_mo('ARACHOVA','-','-').
+archeological_sites_mo('ZAGOROCHORIA','-','-').
+archeological_sites_mo('METSOVO','-','-').
+nightlife_sea('PYLOS','ANO KATO LIVE STAGE','GAZI').
+nightlife_sea('CHALKIDIKI','AQUA BAR','AHOY').
+nightlife_sea('MONEMVASIA','CLUB-VOYAGE','-').
+nightlife_isl('SANTORINI','KOO CLUB','-').
+nightlife_isl('RHODES','COLORADO CLUB','LIVE STAGE').
+nightlife_isl('MYKONOS','MYKONOS BY VOX','-').
+nightlife_mo('ARACHOVA','-','-').
+nightlife_mo('METSOVO','-','-').
+nightlife_mo('ZAGOROCHORIA','-','-').
+nightlife_isl('NAXOS','Island Bar of Naxos','SWING BAR').
+transport_isl('SANTORINI','1)CAR-SHIP','4.5h-9h DURATION OF TRAVEL','TOTAL_COST: 90$','2)MOTORCYCLE-SHIP','TOTAL_COST: 40$','3)TAXI-SHIP','TOTAL_COST:40$','','','','','','','','').
+transport_isl('RHODES','1)CAR-SHIP','11h-26h DURATION OF TRAVEL','TOTAL_COST: 85$','2)MOTORCYCLE-SHIP','TOTAL_COST: 50$','3)TAXI-SHIP','TOTAL_COST: 50$','4)CAR-AIRPLANE','1h-1h&40min. DURATION OF TRAVEL','TOTAL_COST:20$-80$','5)TAXI-AIRPLANE','TOTAL_COST:20$-80$','6)BUS-AIRPLANE','2h-2h&40min. DURATION OF TRAVEL','TOTAL_COST: 20$-80$').
+transport_isl('MYKONOS','1)CAR-SHIP','4h-6h DURATION OF TRAVEL','TOTAL_COST:180$','2)MOTORCYCLE-SHIP','TOTAL_COST: 150$','3)TAXI-SHIP','TOTAL_COST: 150$','','','','','','','','').
+transport_isl('NAXOS','1)CAR-SHIP','4h-5h DURATION OF TRAVEL','TOTAL_COST: 90$','2)MOTORCYCLE-SHIP','TOTAL_COST: 70$','3)TAXI-SHIP','TOTAL_COST: 70$','4)CAR-AIRPLANE','45min.-1h. DURATION OF TRAVEL','TOTAL_COST:20$-80$','6)TAXI-AIRPLANE','TOTAL_COST:20$-80$','7)BUS-AIRPLANE','1h-1h&40min. DURATION OF TRAVEL','TOTAL_COST: 20$-80$').
+transport_sea('MONEMVASIA','1)CAR','3h-4h DURATION OF TRAVEL','TOTAL_COST: 45$','2)MOTORCYCLE','3h-3h&30min. DURATION OF TRAVEL','TOTAL_COST: 40$').
+transport_mo('ARACHOVA','1)CAR','2h DURATION OF TRAVEL','TOTAL_COST: 25$','2)MOTORCYCLE','2h DURATION OF TRAVEL','TOTAL_COST: 15$').
+transport_sea('PYLOS','1)CAR','3h-4h DURATION OF TRAVEL','TOTAL_COST: 60$','2)MOTORCYCLE','3h DURATION OF TARVEL','TOTAL_COST: 40$').
+transport_mo('METSOVO','1)CAR','4h-5h DURATION OF TRAVEL','TOTAL_COST: 90$','2)MOTORCYCLE','4h DURATION OF TRAVEL','TOTAL_COST: 70$').
+transport_mo('ZAGOROCHORIA','1)CAR','5h-6h DURATION OF TRAVEL','TOTAL_COST: 95$','2)MOTORCYCLE','5h DURATION OF TRAVEL','TOTAL_COST: 80$').
+transport_sea('CHALKIDIKI','1)CAR','5h-6h DURATION OF TRAVEL','TOTAL_COST: 90$','2)MOTORCYCLE','5h DURATION OF TRAVEL','TOTAL_COST: 80$').
+hotel_sea('MONEMVASIA','1)MALVASIA TRADITIONAL HOTEL: 16 triple rooms | 20 double rooms | 20 single rooms ','2)BYZANTINO BOUTIQUE HOTEL: 10 triple rooms | 10 double rooms | 11 single rooms').
+hotel_sea('PYLOS','1)GALAXY HOTEL: 10 triple rooms | 20 double rooms | 25 single rooms','2) 12 GODS RESORT: 20 triple rooms | 25 double rooms | 30 single rooms').
+hotel_sea('CHALKIDIKI','1)PELLI HOTEL: 10 triple rooms | 20 double rooms | 25 single rooms','2)SUNSET HOTEL: 20 triple rooms | 21 double rooms | 25 single rooms').
+hotel_mo('ARACHOVA','1)MONT VALLEY BOUTIQUE CHALLETS: 5 triple rooms | 5 double rooms | 6 single rooms','-').
+hotel_mo('ZAGOROCHORIA','1)HOTEL MACHALAS: 2 triple rooms | 4 double rooms | 6 single rooms','-').
+hotel_mo('METSOVO','1)GRAND FOREST METSOVO: 7 triple rooms | 10 double rooms | 10 single rooms','-').
+hotel_isl('MYKONOS','1)POSEIDON HOTEL SUITES: 15 triple rooms | 18 double rooms | 20 single rooms','2)MYKONOS BLANC: 14 triple rooms | 19 double rooms| 23 single rooms').
+hotel_isl('RHODES','1)GEORGIA HOTEL: 10 triple rooms | 20 double rooms | 23 single rooms','2)RODEY HOTEL: 8 triple rooms | 12 double rooms | 20 single rooms').
+hotel_isl('NAXOS','1)XENIA HOTEL: 5 triple rooms | 5 double rooms | 15 single rooms','2)HOTEL GROTTA: 7 triple rooms | 8 double rooms |18 single rooms').
+hotel_isl('SANTORINI','1)PRINCESS PRESIDENTIAL SUITES: 10 triple rooms | 20 double rooms | 25 single rooms','2)SANTORINI HOSTEL: 10 triple rooms | 15 double rooms | 20 single rooms').
+cost_of_rooms_sea('MONEMVASIA','1) MALVASIA TRADITIONAL HOTEL: Triple room 100$ | Double room 80$ | Single room 60$','BYZANTINO BOUTIQUE: Triple room 110$ | Double room 90$ | Single room 70$').
+cost_of_rooms_sea('CHALKIDIKI','1)PELLI HOTEL: Triple room 120$ | Double room 100$ | Single room 90$','2)SUNSET HOTEL: Triple room 115$| Double room 95$ | Single room 85$').
+cost_of_rooms_sea('PYLOS','1)GALAXY HOTEL: Triple room 120$ | Double room 100$ | Single room 95$','2)12 GODS RESORT: Triple room 130$ | Double room 110$ | Single room 100$').
+cost_of_rooms_isl('MYKONOS','1)POSEIDON HOTEL SUITES: 500$ Triple room | 300$ Double room | 200$ Single room','2)MYKONOS BLANC: 450$ Triple room | 350$ Double room | 250$ Single room').
+cost_of_rooms_isl('RHODES','1)GEORGIA HOTEL: Triple room 150$ | Double room 130$ | Single room 110$','RODEY HOTEL: Triple room 160$ | Double room 140$ | Single room 120$').
+cost_of_rooms_isl('NAXOS','1) XENIA HOTEL: Triple room 120$ | Double room 110$ | Single Room 100$','HOTEL GROTTA: Triple room 130$ | Double room 110$ | Single room 105$').
+cost_of_rooms_isl('SANTORINI','1)Princess Presindential Suites: Triple room 450$ | Double room 350$ | Single room 250$','2)Santorini Hostel: Triple room 420$ | Double room 270$ | Single room 200$').
+cost_of_rooms_mo('ARACHOVA','MONT VALLEY BOUTIQUE CHALLETS: Triple room 120$ | Double room 100$ | Single room 80$','-').
+cost_of_rooms_mo('ZAGOROCHORIA','HOTEL MACHALAS: Triple room 100$ | Double room 80$ | Single room 60$','-').
+cost_of_rooms_mo('METSOVO','1)GRAND FOREST METSOVO: Triple room 100$ | Double room 80$ | Single room 60$','-').
+
+
+
+
+
+
+
+
+initial_message:-write('THE DESTINATIONS WHICH INCLUDED IN THIS PROGRAM ARE:'),nl,
+write('1) islands'),nl,
+write('2) mountainous_destinations'),nl,
+write('3) seaside_destinations').
+
+islands:-write('THE ISLANDS ARE:'),nl,
+    write('-NAXOS'),nl,
+    write('-SANTORINI'),nl,
+    write('-RHODES'),nl,
+    write('-MYKONOS'),nl,
+    write('WHICH ISLAND PREFER?'),nl,
+    read(Input),nl,
+    island(Input,Output1,Output2,Output3,Output4),nl,
+    write(Output1),nl,write(Output2),nl,write(Output3),nl,write(Output4),nl,
+    write('THE MUSEUMS ARE:'),nl,
+    museums_isl(Input,Output5,Output6),nl,
+    write(Output5),nl,write(Output6),nl,
+    write('THE ARCHEOLOGICAL SITES ARE:'),nl,
+    archeological_sites_isl(Input,Output7,Output8),nl,
+    write(Output7),nl,write(Output8)
+    ,write('THE NIGHTLIFE IS:'),nl,
+    nightlife_isl(Input,Output9,Output10),nl,write(Output9),nl,write(Output10),
+    nl,write('THE COST OF THIS TRAVEL AND THE TRANSPORTATION IS:'),
+    nl,transport_isl(Input,O1,O2,O3,O4,O5,O6,O7,O8,O9,O10,O11,O12,O13,O14,O15),nl,
+    write(O1),nl,write(O2),nl,write(O3),nl,write(O4),nl,write(O2),nl,write(O5),nl,write(O6),
+    nl,write(O7),nl,write(O2),nl,write(O8),nl,write(O9),nl,write(O10),nl,write(O11),nl,write(O12),
+    nl,write(O13),nl,write(O14),nl,write(O15),nl,
+    write('THE HOTELS AND THEIR CAPACITY:'),nl,hotel_isl(Input,OUTPUT1,OUTPUT2),nl,
+    write(OUTPUT1),nl,write(OUTPUT2),nl,write('THE COST OF ROOMS IN EACH HOTEL:'),nl,
+    cost_of_rooms_isl(Input,OUTPUT3,OUTPUT4),nl,write(OUTPUT3),nl,write(OUTPUT4).
+
+
+
+mountainous_destinations:-write('THE MOUNTAINOUS DESTINATIONS ARE:'),nl,
+    write('-ARACHOVA'),nl,
+    write('-ZAGOROCHORIA'),nl,
+    write('-METSOVO'),nl,
+    write('WHICH MOUNTAINOUS DESTINATIONS PREFER?'),nl,
+    read(Input),nl,
+    mountainous(Input,Output1,Output2,Output3),nl,
+    write(Output1),nl,write(Output2),nl,write(Output3),nl,
+    write('THE MUSEUMS ARE:'),nl,
+    museums_mo(Input,Output4,Output5),nl,
+    write(Output4),nl,write(Output5),nl,
+    write('THE ARCHEOLOGICAL SITES ARE:'),nl,
+    archeological_sites_mo(Input,Output6,Output7),nl,
+    write(Output6),nl,write(Output7),nl,write('THE NIGHTLIFE IS:'),nl,
+    nightlife_mo(Input,Output8,Output9),nl,write(Output8),nl,write(Output9),nl,
+    write('THE COST OF THIS TRAVEL AND TRANSPORTATION IS:'),nl,
+    transport_mo(Input,O1,O2,O3,O4,O5,O6),nl,write(O1),nl,write(O2),nl,write(O3),nl,write(O4),nl,write(O5),nl,write(O6),nl,
+    write('THE HOTELS AND THEIR CAPACITY:'),nl,hotel_mo(Input,OUTPUT1,OUTPUT2),nl,write(OUTPUT1),nl,write(OUTPUT2),nl,
+    write('THE COST OF ROOMS IN EACH HOTEL:'),nl,
+    cost_of_rooms_mo(Input,OUTPUT3,OUTPUT4),nl,write(OUTPUT3),nl,write(OUTPUT4).
+
+
+
+seaside_destinations:-write('THE SEASIDE DESTINATIONS ARE:'),nl,
+    write('-PYLOS'),nl,
+    write('-CHALKIDIKH'),nl,
+    write('-MONEMVASIA'),nl,
+    write('WHICH SEASIDE DESTINATION PREFER?'),nl,
+    read(Input),nl,
+    seaside(Input,Output1,Output2,Output3,Output4),nl,
+    write(Output1),nl,write(Output2),nl,write(Output3),nl,write(Output4)
+   , nl ,write('THE MUSEUMS ARE:'),nl,
+    museums_sea(Input,Output5,Output6),nl,
+    write(Output5),nl,write(Output6),nl,
+    write('THE ARCHEOLOGICAL SITES ARE:'),nl,
+    archeological_sites_sea(Input,Output7,Output8),nl,
+    write(Output7),nl,write(Output8)
+    ,write('THE NIGHTLIFE IS:'),nl,
+    nightlife_sea(Input,Output9,Output10),nl,write(Output9),nl,write(Output10),nl,
+    write('THE COST OF THIS TRAVEL AND TRANSPORTATION IS:'),nl,
+    transport_sea(Input,O1,O2,O3,O4,O5,O6),nl,write(O1),nl,write(O2),nl,write(O3),nl,write(O4),nl,write(O5),nl,write(O6),nl,
+    write('THE HOTELS AND THEIR CAPACITY:'),nl,hotel_sea(Input,OUTPUT1,OUTPUT2),nl,write(OUTPUT1),nl,write(OUTPUT2),nl,
+    write('THE COST IN EACH HOTEL:'),nl,
+   cost_of_rooms_sea(Input,OUTPUT3,OUTPUT4),nl,write(OUTPUT3),nl,write(OUTPUT4) .
+
